65396fc208af208fb5865cbaa78557e01ba72a46
Chris <karampagiaschris@gmail.com>
2023-11-02 20:03:18 +0200

    Migrate from plain fetch to react query

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,8 @@ import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import { Toaster } from "react-hot-toast";
+import QueryProvider from "@/utils/QueryProvider";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 export const metadata = {
   title: "Chris Karampagias' blog",
@@ -19,10 +21,13 @@ export default function RootLayout({ children }) {
   return (
     <html data-theme="winter" lang="en">
       <body className="min-h-screen flex flex-col justify-between">
-        <Navbar />
-        {children}
-        <Footer />
-        <Toaster position="bottom-right" />
+        <QueryProvider>
+          <Navbar />
+          {children}
+          <Footer />
+          <Toaster position="bottom-right" />
+          <ReactQueryDevtools initialIsOpen={false} />
+        </QueryProvider>
       </body>
     </html>
   );
--- utils/apiHooks.js
+++ utils/apiHooks.js
@@ -1,5 +1,6 @@
 "use client";
 import { useState, useEffect } from "react";
+import { useQuery, useMutation } from "@tanstack/react-query";
 
 export const useLatestPosts = () => {
   const [posts, setPosts] = useState(null);
@@ -52,26 +53,24 @@ export const usePosts = () => {
 };
 
 export const usePost = (postId) => {
-  const [post, setPost] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-  useEffect(() => {
-    fetch(`http://localhost:4000/api/posts/${postId}`, {
-      mode: "cors",
-    })
-      .then((res) => {
-        if (!res.ok) {
-          throw new Error("Server error");
-        }
-        return res.json();
+  return useQuery({
+    queryKey: ["post", postId],
+    queryFn: ({ queryKey }) => {
+      return fetch(`http://localhost:4000/api/posts/${queryKey[1]}`, {
+        mode: "cors",
       })
-      .then((postData) => {
-        setPost(postData);
-      })
-      .catch((err) => {
-        setError(err);
-      })
-      .finally(() => setLoading(false));
-  }, []);
-  return [post, error, loading];
+        .then((res) => {
+          if (!res.ok) {
+            throw new Error("Server error");
+          }
+          return res.json();
+        })
+        .catch((err) => {
+          throw new Error(err.message);
+        });
+    },
+    refetchOnReconnect: false,
+    refetchOnWindowFocus: false,
+    refetchInterval: 1000 * 60 * 2,
+  });
 };
